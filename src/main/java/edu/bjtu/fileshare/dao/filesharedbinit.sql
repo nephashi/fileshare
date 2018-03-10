840e7f563affdb95157e269921caed854824aba4
nepha <shi.mohan@foxmail.com>
2018-03-10 15:19:07 +0800

    dbinit

---
+++ src/main/java/edu/bjtu/fileshare/dao/filesharedbinit.sql
@@ -0,0 +1,69 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.6.35 : Database - fileshare
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`fileshare` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `fileshare`;
+
+/*Table structure for table `authorized` */
+
+DROP TABLE IF EXISTS `authorized`;
+
+CREATE TABLE `authorized` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ADDRESS` varchar(50) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `courses` */
+
+DROP TABLE IF EXISTS `courses`;
+
+CREATE TABLE `courses` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(50) NOT NULL,
+  `DEPARTMENT` varchar(50) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `department` */
+
+DROP TABLE IF EXISTS `department`;
+
+CREATE TABLE `department` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(50) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `file` */
+
+DROP TABLE IF EXISTS `file`;
+
+CREATE TABLE `file` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `FILENAME` varchar(100) NOT NULL,
+  `SIZE` float NOT NULL,
+  `COURSE_ID` int(11) NOT NULL,
+  `TEACHER` varchar(30) NOT NULL,
+  `LOCATION` varchar(100) NOT NULL,
+  `DESCRIPTION` varchar(1000) DEFAULT NULL,
+  `UPLOADDATE` date NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
